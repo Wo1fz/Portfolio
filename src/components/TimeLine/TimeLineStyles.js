5d75bdc79417539cb1a60c4c37c759d16cb96b8b
Wo1fz <nmy_97@hotmail.com>
2021-07-28 11:05:42 +0800

    minor style change

--- src/components/TimeLine/TimeLineStyles.js
+++ src/components/TimeLine/TimeLineStyles.js
@@ -65,6 +65,7 @@ export const CarouselItemTitle = styled.h4`
   line-height: 32px;
   letter-spacing: 0.02em;
   display: flex;
+  color: transparent;
   /* This gradient is different due to the size of the Title container, it must transition sooner to be visible on the text */
   background: linear-gradient(
     121.57deg,
@@ -72,6 +73,7 @@ export const CarouselItemTitle = styled.h4`
     rgba(255, 255, 255, 0.66) 30.15%
   );
   -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
   margin-bottom: 8px;
   @media ${(props) => props.theme.breakpoints.md} {
     font-size: 20px;
